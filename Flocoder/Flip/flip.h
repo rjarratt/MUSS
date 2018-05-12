51e2474c6f8e1b90ec310a19a073d8e31411dfb9
Robert Jarratt <rob_j37@hotmail.com>
2018-05-12 14:34:48 +0100

    Corrected headers

--- Flocoder/Flip/flip.h
+++ Flocoder/Flip/flip.h
@@ -1,6 +1,6 @@
 /* flip.h: Form Linear Program
 
-Copyright (c) 2016-2018, Robert Jarratt
+Copyright (c) 2018, Robert Jarratt
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
