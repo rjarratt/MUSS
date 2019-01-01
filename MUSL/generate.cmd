9d1bfc6561f0ce10147cc2c3f8b42ff3f46efc98
Rob <rob:@jarratt.me.uk>
2019-01-01 06:50:57 +0100

    Partial translation of NMSL071

---
+++ MUSL/generate.cmd
@@ -0,0 +1,7 @@
+..\Flocoder\x64\debug\flip nmsl021 ..\..\nmsl\nmsl\nmsl021.h 2
+..\Flocoder\x64\debug\flip nmsl011 ..\..\nmsl\nmsl\nmsl011.c 2
+..\Flocoder\x64\debug\flip nmsl031 ..\..\nmsl\nmsl\nmsl031.c 2
+..\Flocoder\x64\debug\flip nmsl041 ..\..\nmsl\nmsl\nmsl041.c 2
+..\Flocoder\x64\debug\flip nmsl051 ..\..\nmsl\nmsl\nmsl051.c 2
+..\Flocoder\x64\debug\flip nmsl061 ..\..\nmsl\nmsl\nmsl061.c 2
+..\Flocoder\x64\debug\flip nmsl071 ..\..\nmsl\nmsl\nmsl071.c 2
