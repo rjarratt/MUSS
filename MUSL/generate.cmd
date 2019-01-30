9ef7168d1beaaf81c49d8479373a67b52ab06730
Rob <rob:@jarratt.me.uk>
2019-01-30 23:03:17 +0000

    Start of NMSL081

--- MUSL/generate.cmd
+++ MUSL/generate.cmd
@@ -5,3 +5,4 @@
 ..\Flocoder\x64\debug\flip nmsl051 ..\..\nmsl\nmsl\nmsl051.c 2
 ..\Flocoder\x64\debug\flip nmsl061 ..\..\nmsl\nmsl\nmsl061.c 2
 ..\Flocoder\x64\debug\flip nmsl071 ..\..\nmsl\nmsl\nmsl071.c 2
+..\Flocoder\x64\debug\flip nmsl081 ..\..\nmsl\nmsl\nmsl081.c 2
