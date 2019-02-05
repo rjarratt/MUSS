fc59e06d5fadc35b210ebfe92de3b0db51e42ccf
Rob <rob:@jarratt.me.uk>
2019-02-05 22:09:45 +0000

    Change common definitions to be included explicitly and make nmsl021 a proper module

--- MUSL/generate.cmd
+++ MUSL/generate.cmd
@@ -1,4 +1,4 @@
-..\Flocoder\x64\debug\flip nmsl021 ..\..\nmsl\nmsl\nmsl021.h 2
+..\Flocoder\x64\debug\flip nmsl021 ..\..\nmsl\nmsl\nmsl021.c 2
 ..\Flocoder\x64\debug\flip nmsl011 ..\..\nmsl\nmsl\nmsl011.c 2
 ..\Flocoder\x64\debug\flip nmsl031 ..\..\nmsl\nmsl\nmsl031.c 2
 ..\Flocoder\x64\debug\flip nmsl041 ..\..\nmsl\nmsl\nmsl041.c 2
