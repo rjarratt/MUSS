4ddb57c20045ed25f5bdce4e4bf2d318b26a898a
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2019-05-26 09:10:51 +0100

    Added lib switch to build script

--- MUSS/build.cmd
+++ MUSS/build.cmd
@@ -1,3 +1,3 @@
-..\..\nmsl\x64\debug\nmsl.exe consys81.musl consys81.bin
-..\..\nmsl\x64\debug\nmsl.exe ap8011.musl ap8011.bin
-..\..\nmsl\x64\debug\nmsl.exe sys011.musl sys011.bin -i consys81.bin -i ap8011.bin
+..\..\nmsl\x64\debug\nmsl.exe consys81.musl consys81.bin -lib
+..\..\nmsl\x64\debug\nmsl.exe ap8011.musl ap8011.bin -lib
+..\..\nmsl\x64\debug\nmsl.exe sys011.musl sys011.bin -i consys81.bin -i ap8011.bin -lib
