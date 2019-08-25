e658dbdb367cb39755e9d35ca43b0a87473ae12f
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2019-08-25 08:56:21 +0100

    Changed buildref script to use exit codes

--- MUSL/BuildRef.cmd
+++ MUSL/BuildRef.cmd
@@ -1,4 +1,14 @@
 ..\x64\debug\nmsl ..\..\muss\musl\ReferenceUtils.musl ReferenceUtils.bin -lib
+if errorlevel 1 goto error
 ..\x64\debug\nmsl ..\..\muss\musl\ReferenceLibrary1.musl ReferenceLibrary1.bin -lib
+if errorlevel 1 goto error
 ..\x64\debug\nmsl ..\..\muss\musl\ReferenceLibrary2.musl ReferenceLibrary2.bin -lib
+if errorlevel 1 goto error
 ..\x64\debug\nmsl ..\..\muss\musl\ReferenceProgram1.musl ReferenceProgram1.bin -i ReferenceUtils.bin -i ReferenceLibrary1.bin
+
+echo Build succeeded
+goto done
+:error
+echo Build failed
+
+:done
