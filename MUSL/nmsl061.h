e3eed82961c3bec2ec8476bc0cf49b600b612acc
Rob <rob:@jarratt.me.uk>
2019-09-15 14:43:56 +0100

    Moved NMSL061 exports to an include file

---
+++ MUSL/nmsl061.h
@@ -0,0 +1,16 @@
+#pragma once
+
+extern int TRANSCOMP(int);
+extern void TRANSIF(void);
+extern void TRANSWHILE(void);
+extern void TRANSFOR(void);
+extern void TRANSGOTO(void);
+extern void TRANSSWITCH(void);
+extern void TRANSALT(void);
+extern void TRANSELSE(void);
+extern void TRANSFI(void);
+extern void TRANSOD(void);
+extern void TRANSEXIT(void);
+extern void TRANSENDALT(void);
+extern void ALTLABEL(void);
+extern void INITS6(void);
