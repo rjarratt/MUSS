7fcbcf6120fbdc7776f504efce09916fd7305d55
Rob <rob:@jarratt.me.uk>
2019-09-15 18:06:00 +0100

    Moved NMSL091 exports to an include file

---
+++ MUSL/nmsl091.h
@@ -0,0 +1,13 @@
+#pragma once
+
+int FAULTS;
+int32 LINENO;
+int LASTT;
+int LASTP;
+int MAXCH;
+int MAXN;
+int MAXMUTLN;
+
+extern void MONITOR(int);
+extern void INITS9(void);
+extern void PRINTSTATS(void);
