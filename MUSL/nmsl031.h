5621968265eba8bd7ec546ecc764106cea477f27
Rob <rob:@jarratt.me.uk>
2019-09-15 08:50:45 +0100

    Moved NMSL031 exports to an include file

---
+++ MUSL/nmsl031.h
@@ -0,0 +1,46 @@
+#pragma once
+#pragma once
+
+#define TADPROC 0x24
+
+#define TINT0 0x40
+#define TLO8 0x80
+#define TLO0 0x80
+#define TLO1 0x20
+#define STRINGTYPE 0x83
+#define TINT16 0x44
+#define TINT32 0x4C
+#define TINT64 0x5C
+#define TLO32 0x8C
+#define TRE0 0x00
+#define TLAB 0x30
+#define TDELIM 0
+#define TNAME 1
+#define TSTRING 2
+#define TCONST 3
+
+#define IEOS 0
+#define IBYTE 29
+#define IMINCOMP 46
+#define ILOAD 51
+#define ISTORE 61
+#define ICOMP 71
+
+typedef struct { unsigned char TAG, ST; unsigned short IND; } ITYPE;
+
+extern int TINT;
+extern int TVST;
+extern int TLO;
+extern ITYPE LBUFF[LBUFFZ];
+extern int IPTR;
+extern unsigned char CLIST[CLISTZ];
+
+extern const ITYPE UMINUS;
+extern const ITYPE EOS;
+extern const ITYPE LSHIFT;
+extern const ITYPE ASTERISK;
+extern const ITYPE EOL;
+extern const ITYPE DIMPORT;
+
+extern void ITEMISE(int);
+extern void INITS3(void);
