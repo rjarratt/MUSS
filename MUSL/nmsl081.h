5f9d7b8c27f2068ec5bfd95f0837b8816f7d1b1c
Rob <rob:@jarratt.me.uk>
2019-09-15 17:50:03 +0100

    Moved NMSL081 exports to an include file

---
+++ MUSL/nmsl081.h
@@ -0,0 +1,13 @@
+#pragma once
+
+extern int XNPTR;
+extern int LASTN;
+extern int LASTCH;
+
+extern uint16 ADDN(void);
+extern VECTOR *GENN(uint16);
+extern uint16 DECLAREN(uint16, uint16, uint16);
+extern uint16 DUMMY(void);
+extern uint16 MUTLN(uint16);
+extern void EXPN(uint16);
+extern void INITS8(void);
