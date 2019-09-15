5185a01a3221d7dd4eee6a5fbf3d770814592ae3
Rob <rob:@jarratt.me.uk>
2019-09-15 14:58:42 +0100

    Moved NMSL071 exports to an include file

---
+++ MUSL/nmsl071.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define AOPD 0x3000
+#define DIND 0x1004
+#define BREQD -1
+
+uint16 AR[256];
+int AP;
+int FPTR;
+int STPTR;
+
+
+int PREPROCESSCOMP(int);
+void PREPROCESSCOND(void);
+int COMPILECOMP(int, int);
+void COMPILECOND(int, int, int);
+void COMPILEAND(int, int, int);
+void COMPILETEST(int, int, int);
+void PRINTAR(void);
