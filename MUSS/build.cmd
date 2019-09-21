4f5d7f8ed255ab63c6cd1d478a5a12315acdd878
Rob <rob:@jarratt.me.uk>
2019-09-21 18:32:32 +0100

    WIP on getting lowest level coordinator for MU5

--- MUSS/build.cmd
+++ MUSS/build.cmd
@@ -1,3 +1,6 @@
+..\Flocoder\x64\debug\flip sys011 sys011.musl 1
+..\Flocoder\x64\debug\flip sys131 sys131.musl 1
+..\Flocoder\x64\debug\flip sys201 sys201.musl 1
 ..\..\nmsl\x64\debug\nmsl.exe consys81.musl consys81.bin -lib
 ..\..\nmsl\x64\debug\nmsl.exe ap8011.musl ap8011.bin -lib
 ..\..\nmsl\x64\debug\nmsl.exe sys011.musl sys011.bin -i consys81.bin -i ap8011.bin -lib
